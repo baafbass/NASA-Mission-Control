e94f81b0200f9b5710e653e7d0d73a299af942b3
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-03 23:15:30 +0300

    Validation for post request

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,23 @@ function httpGetAllLaunches(req,res){
 
 function httpAddNewLaunch(req,res){
 	const launch  = req.body;
+
+    if(!launch.mission || !launch.rocket || !launch.destination || !launch.launchDate)
+    {
+    	return res.status(400).json({
+    		error:"Launch Missing Required Property",
+    	})
+    }
+
 	launch.launchDate = new Date(launch.launchDate)
+
+	if(isNaN(launch.launchDate))
+      {
+          return res.status(400).json({
+          	error: "Invalid launch Date",
+          })
+      }
+
 	addNewLaunch(launch)
 	return res.status(201).json(launch)
 }
