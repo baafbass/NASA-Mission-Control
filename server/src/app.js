351d3b70b35ac4fdb5f7e3575d260bc200414ae7
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-03 19:20:25 +0300

    Logging Request with Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
 
-app.use(cors({
-	origin: 'http://localhost:3000',
-}))
+app.use(cors({origin: 'http://localhost:3000',}))
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetsRouter)
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 
 module.exports = app;
