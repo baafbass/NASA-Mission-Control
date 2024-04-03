d9b6898d230f56853d5f5cc4c7b6457e8cfcca4f
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-03 14:40:04 +0300

    install the cors middleware

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,8 +1,8 @@
 const API_URL = 'http://localhost:8000';
 
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`)
-  return await response.json()
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
