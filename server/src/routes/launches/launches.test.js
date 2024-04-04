31ce4bebfe95a653e24adb731a0957921c096810
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-04 21:48:08 +0300

    supertest error cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,18 +11,62 @@ describe("Test GET / launches",()=>{
 })
 
 describe("Test POST / launch",()=>{
-	test('Test it should respond with 201 success',async ()=>{
-		const response = await request(app)
-		.post('/launches')
-		.send({
+	
+	const completeLaunchData = {
 			mission:"Niger Aero",
 			target:"kepler-186 f",
 			rocket:"NCC 1701-D",
 			launchDate:"January 23,2025"
-		})
+		}
+
+	const launchDataWithoutDate = {
+			mission:"Niger Aero",
+			target:"kepler-186 f",
+			rocket:"NCC 1701-D"
+		}
+
+	const launchDataWithoutInvalidDate = {
+		    mission:"Niger Aero",
+			target:"kepler-186 f",
+			rocket:"NCC 1701-D",
+			launchDate:"Abdoul Farid"
+	}
+			
+	test('Test it should respond with 201 created',async ()=>{
+		const response = await request(app)
+		.post('/launches')
+		.send(completeLaunchData)
 		.expect('Content-Type',/json/)
-		.expect(201)
+		.expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        
+        expect(responseDate).toBe(requestDate)
+		expect(response.body).toMatchObject(launchDataWithoutDate)
 	})
-	test("It should catch missing required properties",()=>{})
-	test("It should catch invalid dates",()=>{})
+
+
+	test("It should catch missing required properties",async ()=>{
+		const response = await request(app)
+		.post('/launches')
+		.send(launchDataWithoutDate)
+		.expect('Content-Type',/json/)
+		.expect(400)
+
+		expect(response.body).toStrictEqual({
+			error:"Launch Missing Required Property",
+		})
+	})
+	test("It should catch invalid dates",async ()=>{
+		const response = await request(app)
+		.post('/launches')
+		.send(launchDataWithoutInvalidDate)
+		.expect('Content-Type',/json/)
+		.expect(400)
+
+		expect(response.body).toStrictEqual({
+			error: "Invalid launch Date",
+		})
+	})
 })
