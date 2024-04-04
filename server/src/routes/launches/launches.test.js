328248f57f3f0a16016d383ed7e6056814549a61
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-04 21:07:42 +0300

    Testing APIs with jest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,12 @@
+const request = require('supertest')
+const app = require('../../app.js')
+
 describe("Test GET / launches",()=>{
-	test("It Should respond with 200 success",()=>{
-		const response = 200;
-		expect(response).toBe(200);
+	test("It Should respond with 200 success",async ()=>{
+		const response = await request(app)
+		.get('/launches')
+		.expect('Content-Type',/json/)
+		.expect(200);
 	})
 })
 
