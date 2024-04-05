b9a9ef0952c1b8df3fc2ad1bc445e0d432c9d53b
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-05 21:58:42 +0300

    Upsert the planet data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -12,7 +12,6 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
@@ -22,27 +21,44 @@ function loadPlanetsData() {
       }))
       .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          //TODO: Replace bellow create with insert + update = upsert
-
-          // await planets.create({
-          //   keplerName:data.kepler_name,
-          // });
+          savePlanet(data);
         }
       })
       .on('error', (err) => {
         console.log(err);
         reject(err);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       });
   });
 }
 
 async function getAllPlanets(){
-  return await planets.find();
+  return await planets.find({});
 }
+
+async function savePlanet(planet)
+  {  
+
+    try{
+await planets.updateOne(
+    {
+              keplerName:planet.kepler_name,
+            },
+            {
+              keplerName:planet.kepler_name,
+            },
+            {
+              upsert:true,
+            });
+    }catch(err){
+       console.error(`Could not save a planet ${err}`)
+    }
+
+  }
 
 module.exports = {
   loadPlanetsData,
