c4528903e8e1f2fdcf3f063472f04dea7ace1100
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-08 21:38:20 +0300

    Mapping SpaceX Data to the Database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -43,7 +43,24 @@ async function loadLaunchData(){
 })
 
 
-	
+	const launchDocs = response.data.docs;
+	for (const launchDoc of launchDocs){
+        const payloads = launchDoc['payloads']
+        const customers = payloads.flatMap((payload)=>{
+        	return payload['customers']
+        })
+
+       const launch = {
+       	flightNumber: launchDoc['flight_number'],
+       	mission: launchDoc['name'],
+       	rocket: launchDoc['rocket']['name'],
+       	launchDate: launchDoc['date_local'],
+       	upcoming: launchDoc['upcoming'],
+       	success: launchDoc['success'],
+       	customers,
+       }
+       console.log(`${launch.flightNumber} --> ${launch.mission}`)
+	}
 }
 
 async function existLaunchWithId(LaunchId){
