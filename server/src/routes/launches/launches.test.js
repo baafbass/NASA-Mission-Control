a3d776baf6bbfac086175b1b5fe9a34ea6b61b3e
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-11 15:28:51 +0300

    Pass client test with no test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,13 @@
 const request = require('supertest')
 const app = require('../../app.js')
 const {mongoConnect,mongoDisconnect} = require('../../services/mongo')
+const {loadPlanetsData} =require('../../models/planets.model')
 
 describe("Launches API",()=>{
 
 beforeAll(async ()=>{
 	await mongoConnect();
+	await loadPlanetsData();
 })
 
 afterAll(async ()=>{
