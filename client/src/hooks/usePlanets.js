b61114a3bdb7f02473e081dd1e4878281da3fbdb
Farid <abdoulfaridbassirou7898@gmail.com>
2024-04-11 14:05:59 +0300

    using the babel devdep and fix the use usePlanets warnings

--- client/src/hooks/usePlanets.js
+++ client/src/hooks/usePlanets.js
@@ -1,3 +1,25 @@
+// import { useCallback, useEffect, useState } from "react";
+
+// import { httpGetPlanets } from "./requests";
+
+// function usePlanets() {
+//   const [planets, savePlanets] = useState([]);
+
+//   const getPlanets = useCallback(async () => {
+//     const fetchedPlanets = await httpGetPlanets();
+//     savePlanets(fetchedPlanets);
+//   }, []);
+
+//   useEffect(() => {
+//     getPlanets();
+//     console.log(planets)
+//   }, [getPlanets]);
+
+//   return planets;
+// }
+
+// export default usePlanets;
+
 import { useCallback, useEffect, useState } from "react";
 
 import { httpGetPlanets } from "./requests";
@@ -11,9 +33,13 @@ function usePlanets() {
   }, []);
 
   useEffect(() => {
-    getPlanets();
-    console.log(planets)
-  }, [getPlanets]);
+    const fetchData = async () => {
+      await getPlanets();
+      console.log(planets);
+    };
+
+    fetchData();
+  }, [getPlanets, planets]); // Include getPlanets and planets in the dependency array
 
   return planets;
 }
