2c52852b6eb11fc8ba63ef7234c021cfc5c5c037
Farid <abdoulfaridbassirou7898@gmail.com>
2024-05-23 17:16:12 +0300

    App deployed to aws/ec2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return <ArwesFooter animate>
     <Centered>
       <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
-        This is not an official site and is not affiliated with NASA or SpaceX in any way. For educational purposes only.
+        This is not an official site and is not affiliated with NASA or SpaceX in any way.
       </Paragraph>
     </Centered>
   </ArwesFooter>
